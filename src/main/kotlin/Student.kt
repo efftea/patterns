535b9089f51dc2699e57e70480282dacb789df58
efftea <131270184+efftea@users.noreply.github.com>
2024-11-26 11:03:01 +0300

    Выполнено 1.1

---
+++ src/main/kotlin/Student.kt
@@ -0,0 +1,82 @@
+class Student{
+    var id: Int = 0
+        set(value)
+        {
+            if(value>0)
+                field=value
+        }
+        get()
+        {
+            return field
+        }
+
+    var lastName: String = ""
+        set(value)
+        {
+            field=value
+        }
+        get()
+        {
+            return field
+        }
+
+    var firstName: String = ""
+        set(value)
+        {
+            field=value
+        }
+        get()
+        {
+            return field
+        }
+
+    var middleName: String? = null
+        set(value)
+        {
+            field=value
+        }
+        get()
+        {
+            return field
+        }
+
+    var phone: String? = null
+        set(value)
+        {
+            field=value
+        }
+        get()
+        {
+            return field
+        }
+
+    var telegram: String? = null
+        set(value)
+        {
+            field=value
+        }
+        get()
+        {
+            return field
+        }
+
+    var email: String? = null
+        set(value)
+        {
+            field=value
+        }
+        get()
+        {
+            return field
+        }
+
+    var github: String? = null
+        set(value)
+        {
+            field=value
+        }
+        get()
+        {
+            return field
+        }
+}
