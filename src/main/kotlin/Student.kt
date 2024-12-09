5aadf290aae10e47a137f9d77e6da4051830944d
efftea <131270184+efftea@users.noreply.github.com>
2024-12-09 03:34:56 +0300

    Выполнено 2.7.  Написан метод read_from_txt.

--- src/main/kotlin/Student.kt
+++ src/main/kotlin/Student.kt
@@ -1,3 +1,7 @@
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+
 class Student: SuperStudentClass{
     var lastName: String = ""
         set(value)
@@ -112,6 +116,29 @@ class Student: SuperStudentClass{
         if(_mail!=null&&validateEMail(_mail))
         {
             email = _mail
+        }
+    }
+
+    companion object
+    {
+        fun read_from_txt(path:String): MutableList<Student>
+        {
+            val file = File(path)
+            var res = mutableListOf<Student>()
+            var text:List<String> = listOf()
+            try {
+                text = file.readLines()
+                println(text)
+            } catch (e: FileNotFoundException) {
+                println("Файл не найден")
+            } catch (e: IOException) {
+                println("Ошибка при прочтении файла.")
+            }
+            for (line in text)
+            {
+                res.add(Student(line))
+            }
+            return res
         }
     }
 
